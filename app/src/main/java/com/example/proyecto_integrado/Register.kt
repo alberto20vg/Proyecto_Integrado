f4601c709c883ff620d0fd810b41ab36befcffb8
alberto20vg <alberto30vg@gmail.com>
2021-04-14 21:28:10 +0200

    Register/ImageController: creacion de intent para subir foto

--- app/src/main/java/com/example/proyecto_integrado/Register.kt
+++ app/src/main/java/com/example/proyecto_integrado/Register.kt
@@ -1,22 +1,25 @@
 package com.example.proyecto_integrado
 
+import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toFile
 import com.example.proyecto_integrado.ui.theme.Proyecto_IntegradoTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -24,13 +27,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.StorageReference
+
+import com.google.firebase.storage.FirebaseStorage
+import com.google.android.gms.tasks.OnSuccessListener
+
+
+
+
+
+
+
 
 class Register : ComponentActivity() {
     val RC_SIGN_IN = 1664
     private var mauth = Firebase.auth
     private lateinit var googleSignInClient: GoogleSignInClient
     val db = Firebase.firestore
-
+    val storage = Firebase.storage
+    var storageRef = storage.reference
+    val SELECT_ACTIVITY = 50
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,6 +69,16 @@ class Register : ComponentActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when {
+            requestCode == SELECT_ACTIVITY && resultCode == Activity.RESULT_OK -> {
+                imageUri = data!!.data
+            }
+        }
+    }
+
     @Composable
     fun register() {
         val context = LocalContext.current
@@ -62,14 +90,59 @@ class Register : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            val imagen by remember { mutableStateOf((R.drawable.ic_launcher_foreground)) }
-            Image(painterResource(imagen), contentDescription = null)
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
             Button(onClick = {
                 //TODO quitar logout y hacer intent registro
                 Toast.makeText(context, "cambiar boton subir imagen", Toast.LENGTH_SHORT).show()
+
+                ImageController.selectPhotoFromGallery(this@Register, SELECT_ACTIVITY)
+
+                var file = imageUri
+                    //Uri.fromFile(File("Internal storage/WhatsApp/Media/Whatsapp Images/IMG-20210413-WA0002.jpg"))
+                val riversRef = storageRef.child("images/${file}")
+                var uploadTask = file?.let { riversRef.putFile(it) }
+
+            // Register observers to listen for when the download is done or if it fails
+                if (uploadTask != null) {
+                    uploadTask.addOnFailureListener {
+                        // Handle unsuccessful uploads
+                    }.addOnSuccessListener {
+                        // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                        // ...
+                    }
+                }
+
+
             }) { Text(getString(R.string.upload_potho)) }
+
             Spacer(modifier = Modifier.height(16.dp))
+            
+            Button(onClick = {
+
+                Toast.makeText(context, imageUri?.path, Toast.LENGTH_SHORT).show()
+                var str = imageUri?.path
+                if (str != null) {
+                    str = str.replace("raw/", "images")
+                }
+                Toast.makeText(context, str, Toast.LENGTH_SHORT).show()
+
+                /*
+                val storage = FirebaseStorage.getInstance()
+                val storageRef = storage.getReferenceFromUrl("gs://proyecto-integrado-8b304.appspot.com")
+                    .child("images/${imageUri.toString()}")
+                        //"images/storage/emulated/0/Music/2018 - When the Curtain Falls (Single)/2018 - When the Curtain Falls (Single) 01.jpg")
+
+                storageRef.downloadUrl.addOnSuccessListener { uri ->
+                    Toast.makeText(context, uri.toString(), Toast.LENGTH_SHORT).show()
+                    //Handle whatever you're going to do with the URL here
+                }
+
+                 */
+
+            })  { Text("Prueba Url") }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
 
             var error by remember { mutableStateOf(false) }
