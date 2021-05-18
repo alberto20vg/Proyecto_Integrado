adcda31423c41661e5ed816fd9a7262593a8f3a1
Alberto Valdivia <albertovaldivia@guadaltel.com>
2021-05-18 13:16:03 +0200

    eliminacion de recyclerPrueba

--- app/src/main/java/com/example/proyecto_integrado/Components/BooksResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto_integrado.Components
-
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.QuerySnapshot
-
-sealed class BooksResponse
-data class OnSuccess(val querySnapshot: QuerySnapshot?): BooksResponse()
-data class OnError(val exception: FirebaseFirestoreException?): BooksResponse()
